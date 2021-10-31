fefc9ca3479f9fed7fbd6cb26750652624ce0efd
Tang Sicheng <tangsicheng@hotmail.com>
2021-10-31 17:02:13 +0800

    change address

--- CloudProviderMonitor/env/env.go
+++ CloudProviderMonitor/env/env.go
@@ -2,4 +2,4 @@ package env
 
 const KovanInfura = "wss://kovan.infura.io/ws/v3/2ff48848206746749e091cbdc4fc24c2"
 const PrivateKey = "fb893fcb257ccf36cb614fd6614c8fa62664e52cfb0fdb880e9d7794cb67e7cb"
-const ContractAddress = "0x67Ce026Ea4d723f0e3476B8E0F56A68943420BA1"
+const ContractAddress = "0xc2Ca4067a6774A0123F45767616e5cd88007F4E2"
